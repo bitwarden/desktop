583c6fcad6dc78fc962a453311313350d9f077a4
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-09-07 23:07:07 -0400

    new layout for tabs and header

---
+++ src/popup/app/global/globalModule.js
@@ -0,0 +1,2 @@
+﻿angular
+    .module('bit.global', []);
---
+++ src/popup/app/global/mainController.js
@@ -0,0 +1,10 @@
+﻿angular
+    .module('bit.global')
+
+    .controller('mainController', function ($scope, $state) {
+        $scope.currentYear = new Date().getFullYear();
+
+        $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            
+        });
+    });
