1b8df50f27ac566820be9cc2c301fd63183f2121
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-09-19 22:48:42 -0400

    color and link tools options

--- src/popup/app/tools/toolsController.js
+++ src/popup/app/tools/toolsController.js
@@ -2,5 +2,15 @@
     .module('bit.tools')
 
     .controller('toolsController', function ($scope) {
+        $scope.launchWebVault = function () {
+            chrome.tabs.create({ url: 'https://vault.bitwarden.com' });
+        };
 
+        $scope.launchiOS = function () {
+            chrome.tabs.create({ url: 'https://itunes.apple.com/us/app/bitwarden-free-password-manager/id1137397744?mt=8' });
+        };
+
+        $scope.launchAndroid = function () {
+            chrome.tabs.create({ url: 'https://play.google.com/store/apps/details?id=com.x8bit.bitwarden' });
+        };
     });
