db80727e1e6fd6b8a8f425fed4af1d522fb93d39
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-09-21 17:24:19 -0400

    list folders

--- src/popup/app/settings/settingsFoldersController.js
+++ src/popup/app/settings/settingsFoldersController.js
@@ -1,6 +1,15 @@
 ﻿angular
     .module('bit.settings')
 
-    .controller('settingsFoldersController', function ($scope) {
-        
+    .controller('settingsFoldersController', function ($scope, folderService, $q) {
+        $scope.loaded = false;
+
+        load();
+        function load() {
+            var foldersPromise = $q.when(folderService.getAllDecrypted());
+            foldersPromise.then(function (folders) {
+                $scope.loaded = true;
+                $scope.folders = folders;
+            });
+        }
     });
