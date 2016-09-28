deb3cbeb48c214a0cd7ca912e98c3edd3dc310c9
Kyle Spearrin <kspearrin@imobile3.com>
2016-09-27 23:35:07 -0400

    added version dimension to analytics tracking

--- src/scripts/analytics.js
+++ src/scripts/analytics.js
@@ -18,6 +18,9 @@ if (gaTrackingId) {
     ga('create', gaTrackingId, 'auto');
 }
 
+// version dimension
+ga('set', 'dimension1', chrome.runtime.getManifest().version);
+
 // Removes failing protocol check. ref: http://stackoverflow.com/a/22152353/1958200
 ga('set', 'checkProtocolTask', function () { });
 
