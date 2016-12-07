4541185a5115a48d55482718115f8f43e840502e
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-12-07 00:12:16 -0500

    state service

---
+++ src/popup/app/services/stateService.js
@@ -0,0 +1,21 @@
+﻿angular
+    .module('bit.services')
+
+    .factory('stateService', function () {
+        var _service = {},
+            _state = {};
+
+        _service.saveState = function (key, data) {
+            _state[key] = data;
+        };
+
+        _service.getState = function (key) {
+            if (key in _state) {
+                return _state[key];
+            }
+
+            return null;
+        };
+
+        return _service;
+    });
