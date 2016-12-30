77aca102e64df5407563795e8fb953556a283ed4
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-12-30 16:47:07 -0500

    styling the notification bar some

--- src/content/notificationBar.js
+++ src/content/notificationBar.js
@@ -122,17 +122,17 @@
 
         var iframe = document.createElement('iframe');
         iframe.src = chrome.extension.getURL(barPage);
-        iframe.style.cssText = 'height: 41px; width: 100%; border: 0;';
+        iframe.style.cssText = 'height: 42px; width: 100%; border: 0;';
 
         var frameDiv = document.createElement('div');
         frameDiv.id = 'bit-notification-bar';
-        frameDiv.style.cssText = 'height: 41px; width: 100%; top: 0; left: 0; padding: 0; position: fixed; z-index: 1000000099; visibility: visible;';
+        frameDiv.style.cssText = 'height: 42px; width: 100%; top: 0; left: 0; padding: 0; position: fixed; z-index: 1000000099; visibility: visible;';
         frameDiv.appendChild(iframe);
         document.body.appendChild(frameDiv);
 
         var spacer = document.createElement('div');
         spacer.id = 'bit-notification-bar-spacer';
-        spacer.style.cssText = 'height: 41px;';
+        spacer.style.cssText = 'height: 42px;';
         document.body.insertBefore(spacer, document.body.firstChild);
     }
 
