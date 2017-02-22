d06744f73960a533551819b39a054a82a43d4f2d
Kyle Spearrin <kspearrin@imobile3.com>
2017-02-22 18:58:31 -0500

    Added dist-firefox gulp task to add applications section to manifest

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     googleWebFonts = require('gulp-google-webfonts'),
-    webpack = require('webpack-stream');
+    webpack = require('webpack-stream')
+    jeditor = require("gulp-json-editor");
 
 var paths = {};
 paths.dist = './dist/';
@@ -202,6 +203,20 @@ gulp.task('dist', ['build'], function (cb) {
         'dist:clean',
         'dist:move',
         cb);
+});
+
+gulp.task('dist-firefox', ['dist'], function (cb) {
+    gulp.src(paths.dist + 'manifest.json')
+      .pipe(jeditor(function (manifest) {
+          manifest.applications = {
+              gecko: {
+                  id: "addon@bitwarden.com",
+                  strict_min_version: "42.0"
+              }
+          };
+          return manifest;
+      }))
+      .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('webfonts', function () {
