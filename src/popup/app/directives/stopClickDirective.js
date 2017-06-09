5409ba77370bec03197a160f0bbc02db612ec3a5
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-06-09 15:14:36 -0400

    stop click directive instead of inline js

---
+++ src/popup/app/directives/stopClickDirective.js
@@ -0,0 +1,11 @@
+﻿angular
+    .module('bit.directives')
+
+    // ref: https://stackoverflow.com/a/14165848/1090359
+    .directive('stopClick', function () {
+        return function (scope, element, attrs) {
+            $(element).click(function (event) {
+                event.preventDefault();
+            });
+        };
+    });
