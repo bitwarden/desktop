842e594ba26c6677bd07fd939524c4488200ecfd
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-11-22 12:38:58 -0500

    remove grouped view of items in vault

--- src/popup/app/vault/vaultController.js
+++ src/popup/app/vault/vaultController.js
@@ -8,7 +8,6 @@ angular
 
         $scope.i18n = i18nService;
         $scope.showFolderCounts = !utilsService.isEdge();
-        $scope.showOnlyFolderView = utilsService.isEdge();
         $scope.disableSearch = utilsService.isEdge();
         document.getElementById('search').focus();
 
