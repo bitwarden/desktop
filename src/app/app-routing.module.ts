d90d26dcb678817c9386d2484689b00490d8eb0c
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-01-20 23:45:22 -0500

    vault flex layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { LoginComponent } from './accounts/login.component';
 import { VaultComponent } from './vault/vault.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/vault', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'vault', component: VaultComponent },
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     styles: [],
-    template: 'App<br /><router-outlet></router-outlet>',
+    template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
 }
