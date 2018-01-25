f5459f4c857bc950f9bac8bf162160f0ea816957
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-01-25 16:15:47 -0500

    ui elements for add cipher

--- src/app/vault/add.component.ts
+++ src/app/vault/add.component.ts
@@ -7,12 +7,16 @@ import {
 } from '@angular/core';
 
 import { CipherType } from 'jslib/enums/cipherType';
+import { FieldType } from 'jslib/enums/fieldType';
 import { SecureNoteType } from 'jslib/enums/secureNoteType';
 
 import { CipherService } from 'jslib/abstractions/cipher.service';
+import { FolderService } from 'jslib/abstractions/folder.service';
+import { I18nService } from 'jslib/abstractions/i18n.service';
 
 import { CardView } from 'jslib/models/view/cardView';
 import { CipherView } from 'jslib/models/view/cipherView';
+import { FolderView } from 'jslib/models/view/folderView';
 import { IdentityView } from 'jslib/models/view/identityView';
 import { LoginView } from 'jslib/models/view/loginView';
 import { SecureNoteView } from 'jslib/models/view/secureNoteView';
@@ -24,8 +28,56 @@ import { SecureNoteView } from 'jslib/models/view/secureNoteView';
 export class AddComponent implements OnChanges {
     @Input() folderId: string;
     cipher: CipherView;
+    folders: FolderView[];
+    cipherType = CipherType;
+    fieldType = FieldType;
+    typeOptions: any[];
+    cardBrandOptions: any[];
+    cardExpMonthOptions: any[];
+    identityTitleOptions: any[];
 
-    constructor(private cipherService: CipherService) {
+    constructor(private cipherService: CipherService, private folderService: FolderService,
+        private i18nService: I18nService) {
+        this.typeOptions = [
+            { name: i18nService.t('typeLogin'), value: CipherType.Login },
+            { name: i18nService.t('typeCard'), value: CipherType.Card },
+            { name: i18nService.t('typeIdentity'), value: CipherType.Identity },
+            { name: i18nService.t('typeSecureNote'), value: CipherType.SecureNote },
+        ];
+        this.cardBrandOptions = [
+            { name: '-- ' + i18nService.t('select') + ' --', value: null },
+            { name: 'Visa', value: 'Visa' },
+            { name: 'Mastercard', value: 'Mastercard' },
+            { name: 'American Express', value: 'Amex' },
+            { name: 'Discover', value: 'Discover' },
+            { name: 'Diners Club', value: 'Diners Club' },
+            { name: 'JCB', value: 'JCB' },
+            { name: 'Maestro', value: 'Maestro' },
+            { name: 'UnionPay', value: 'UnionPay' },
+            { name: i18nService.t('other'), value: 'Other' },
+        ];
+        this.cardExpMonthOptions = [
+            { name: '-- ' + i18nService.t('select') + ' --', value: null },
+            { name: '01 - ' + i18nService.t('january'), value: '1' },
+            { name: '02 - ' + i18nService.t('february'), value: '2' },
+            { name: '03 - ' + i18nService.t('march'), value: '3' },
+            { name: '04 - ' + i18nService.t('april'), value: '4' },
+            { name: '05 - ' + i18nService.t('may'), value: '5' },
+            { name: '06 - ' + i18nService.t('june'), value: '6' },
+            { name: '07 - ' + i18nService.t('july'), value: '7' },
+            { name: '08 - ' + i18nService.t('august'), value: '8' },
+            { name: '09 - ' + i18nService.t('september'), value: '9' },
+            { name: '10 - ' + i18nService.t('october'), value: '10' },
+            { name: '11 - ' + i18nService.t('november'), value: '11' },
+            { name: '12 - ' + i18nService.t('december'), value: '12' },
+        ];
+        this.identityTitleOptions = [
+            { name: '-- ' + i18nService.t('select') + ' --', value: null },
+            { name: i18nService.t('mr'), value: i18nService.t('mr') },
+            { name: i18nService.t('mrs'), value: i18nService.t('mrs') },
+            { name: i18nService.t('ms'), value: i18nService.t('ms') },
+            { name: i18nService.t('dr'), value: i18nService.t('dr') },
+        ];
     }
 
     async ngOnChanges() {
@@ -37,5 +89,7 @@ export class AddComponent implements OnChanges {
         this.cipher.identity = new IdentityView();
         this.cipher.secureNote = new SecureNoteView();
         this.cipher.secureNote.type = SecureNoteType.Generic;
+
+        this.folders = await this.folderService.getAllDecrypted();
     }
 }
