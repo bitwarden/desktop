4336212a007b54a46ee026757e324aac4630f518
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-02-15 14:05:37 -0500

    config for storage

--- src/services/desktopStorage.service.ts
+++ src/services/desktopStorage.service.ts
@@ -1,8 +1,23 @@
-import { StorageService } from 'jslib/abstractions';
+import { StorageService } from 'jslib/abstractions/storage.service';
+
+import { ConstantsService } from 'jslib/services/constants.service';
 
 // tslint:disable-next-line
 const Store = require('electron-store');
-const store = new Store();
+
+const storeConfig: any = {
+    defaults: {} as any,
+    name: 'bitwarden-data',
+};
+
+// Default lock options to "on restart".
+storeConfig.defaults[ConstantsService.lockOptionKey] = -1;
+// Portable builds should not use app data
+if (process.env.PORTABLE_EXECUTABLE_DIR != null) {
+    storeConfig.cwd = process.env.PORTABLE_EXECUTABLE_DIR;
+}
+
+const store = new Store(storeConfig);
 
 export class DesktopStorageService implements StorageService {
     get<T>(key: string): Promise<T> {
