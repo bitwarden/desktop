456a091c0569415358957dafddc5d046112c9245
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-02-19 23:33:43 -0500

    fixes to cask-update script

--- scripts/cask-update.sh
+++ scripts/cask-update.sh
@@ -10,23 +10,21 @@ DIST_DIR="$ROOT_DIR/dist"
 CASK_DIR="$ROOT_DIR/stores/homebrew-cask"
 DIST_CASK_DIR="$DIST_DIR/homebrew-cask"
 
-cp -r $CASK_DIR $DIST_CASK_DIR
-
-if [ ! -d "$DIST_CASK_DIR" ]
+if [ -d "$DIST_CASK_DIR" ]
 then
     rm -rf $DIST_CASK_DIR
 fi
 
-cp -r $CASK_DIR $DIST_CASK_DIR
+cp -r $CASK_DIR $DIST_DIR
 
 SRC_PACKAGE="$ROOT_DIR/src/package.json";
-SRC_PACAKGE_VERSION=(jq 'version' SRC_PACKAGE)
+SRC_PACAKGE_VERSION=$(jq -r '.version' $SRC_PACKAGE)
 
-ZIP="$DIST_DIR\Bitwarden-$SRC_PACAKGE_VERSION-mac.zip"
-CHECKSUM=(shasum -a 256 $ZIP)
-CHECKPOINT=(brew cask _appcast_checkpoint --calculate "https://github.com/bitwarden/desktop/releases.atom")
+ZIP="$DIST_DIR/Bitwarden-$SRC_PACAKGE_VERSION-mac.zip"
+CHECKSUM=($(shasum -a 256 $ZIP))
+CHECKPOINT=$(brew cask _appcast_checkpoint --calculate "https://github.com/bitwarden/desktop/releases.atom")
 RB="$DIST_CASK_DIR/bitwarden.rb"
+RB_NEW="$DIST_CASK_DIR/bitwarden.rb.new"
 
-sed -i -e 's/__version__/'"$SRC_PACAKGE_VERSION"'/g' $RB
-sed -i -e 's/__checksum__/'"$CHECKSUM"'/g' $RB
-sed -i -e 's/__checkpoint__/'"$CHECKPOINT"'/g' $RB
+sed -e 's/__version__/'"$SRC_PACAKGE_VERSION"'/g; s/__checksum__/'"$CHECKSUM"'/g; s/__checkpoint__/'"$CHECKPOINT"'/g' $RB > $RB_NEW
+mv -f $RB_NEW $RB
