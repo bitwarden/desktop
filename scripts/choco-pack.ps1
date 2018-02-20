b0f6105ae67779acb79a74e31ccc1961ad5dc84f
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-02-19 21:56:46 -0500

    choco pack script

---
+++ scripts/choco-pack.ps1
@@ -0,0 +1,8 @@
+﻿$dir = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$rootDir = $dir + "\..";
+$distDir = $rootDir + "\dist";
+$nuspec = $rootDir + "\stores\chocolatey\bitwarden.nuspec";
+$srcPackage = $rootDir + "\src\package.json";
+$srcPackageVersion = (Get-Content -Raw -Path $srcPackage | ConvertFrom-Json).version;
+
+choco pack $nuspec --version $srcPackageVersion --out $distDir
