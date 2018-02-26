bf559f363bb7f68d75482d46c8ff7e41567cb855
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-02-25 21:16:31 -0500

    remove old fs import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { app, BrowserWindow } from 'electron';
-import * as fs from 'fs';
 import * as path from 'path';
 
 import { DesktopMainMessagingService } from './services/desktopMainMessaging.service';
