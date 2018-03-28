b24520fcead61c967d0d1509e9cc4c72367d497f
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-03-28 16:26:20 -0400

    change out of directory before deleting

--- ci-build.cmd
+++ ci-build.cmd
@@ -20,7 +20,7 @@ CALL git clone --branch=%APPVEYOR_REPO_BRANCH% %REPO_URL% %DIST_SRC_DIR%
 cd %DIST_SRC_DIR%
 CALL git checkout %APPVEYOR_REPO_COMMIT%
 CALL git submodule update --init --recursive
-DEL /S/Q "%DIST_SRC_DIR%.git\objects\pack\*"
 cd %DIST_DIR%
+DEL /S/Q "%DIST_SRC_DIR%.git\objects\pack\*"
 CALL 7z a browser-source-%APPVEYOR_BUILD_NUMBER%.zip "%DIST_SRC_DIR%\*"
 cd %APPVEYOR_BUILD_FOLDER%
