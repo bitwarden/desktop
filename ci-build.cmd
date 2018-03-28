8ac1c03033ec9370528607083e3248c87686aa73
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-03-28 15:43:43 -0400

    ci build script npm dist too

--- ci-build.cmd
+++ ci-build.cmd
@@ -6,6 +6,7 @@ SET DIST_DIR=%APPVEYOR_BUILD_FOLDER%\dist\
 SET DIST_SRC_DIR=%DIST_DIR%Source\
 SET REPO_URL=https://github.com/%APPVEYOR_REPO_NAME%.git
 
+call npm run dist
 call git clone %REPO_URL% %DIST_SRC_DIR%
 cd %DIST_SRC_DIR%
 call git checkout %APPVEYOR_REPO_COMMIT%
