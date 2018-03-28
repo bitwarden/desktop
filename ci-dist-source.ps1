5b83a0ea0a8ab4e101f439378b6b937fc2044c42
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-03-28 14:11:18 -0400

    ci script for package source for dist

---
+++ ci-dist-source.ps1
@@ -0,0 +1,9 @@
+﻿$distDir = $env:APPVEYOR_BUILD_FOLDER + "\dist\"
+$distSrcDir = $distDir + "Source\"
+git clone https://github.com/ + $env:APPVEYOR_REPO_NAME + ".git" $distSrcDir
+cd $distSrcDir
+git checkout $env:APPVEYOR_REPO_COMMIT
+git submodule update --init --recursive
+cd $distSrcDir
+7z a browser-source-$env:APPVEYOR_BUILD_NUMBER.zip $distSrcDir
+cd $env:APPVEYOR_BUILD_FOLDER
