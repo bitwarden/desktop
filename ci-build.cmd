3acaa6e87aab84d3686b3e3d2aee9b2415edf1cd
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-03-28 15:53:01 -0400

    comments

--- ci-build.cmd
+++ ci-build.cmd
@@ -6,11 +6,16 @@ SET DIST_DIR=%APPVEYOR_BUILD_FOLDER%\dist\
 SET DIST_SRC_DIR=%DIST_DIR%Source\
 SET REPO_URL=https://github.com/%APPVEYOR_REPO_NAME%.git
 
-call npm run dist
-call git clone %REPO_URL% %DIST_SRC_DIR%
+:: Do normal build
+ECHO "Build dist"
+CALL npm run dist
+
+:: Build sources for reviewers
+ECHO "Build sources"
+CALL git clone --branch=%APPVEYOR_REPO_BRANCH% %REPO_URL% %DIST_SRC_DIR%
 cd %DIST_SRC_DIR%
-call git checkout %APPVEYOR_REPO_COMMIT%
-call git submodule update --init --recursive
+CALL git checkout %APPVEYOR_REPO_COMMIT%
+CALL git submodule update --init --recursive
 cd %DIST_DIR%
-call 7z a browser-source-%APPVEYOR_BUILD_NUMBER%.zip %DIST_SRC_DIR%\*
+CALL 7z a browser-source-%APPVEYOR_BUILD_NUMBER%.zip %DIST_SRC_DIR%\*
 cd %APPVEYOR_BUILD_FOLDER%
