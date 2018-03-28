b7b43406e8a3e7fa15a846b0f11aac0ed80a7930
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-03-28 14:17:27 -0400

    repo url variable

--- ci-dist-source.ps1
+++ ci-dist-source.ps1
@@ -1,8 +1,9 @@
 ﻿$distDir = $env:APPVEYOR_BUILD_FOLDER + "\dist\"
 $distSrcDir = $distDir + "Source\"
-git clone https://github.com/ + $env:APPVEYOR_REPO_NAME + ".git" $distSrcDir
+$repoUrl = "https://github.com/" + $env:APPVEYOR_REPO_NAME + ".git"
+git clone -q --branch=master $repoUrl $distSrcDir
 cd $distSrcDir
-git checkout $env:APPVEYOR_REPO_COMMIT
+git checkout -qf $env:APPVEYOR_REPO_COMMIT
 git submodule update --init --recursive
 cd $distSrcDir
 7z a browser-source-$env:APPVEYOR_BUILD_NUMBER.zip $distSrcDir
