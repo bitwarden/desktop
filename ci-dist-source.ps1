949224339b4f9f73ce2668286b0b28af1138b78c
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-03-28 14:32:41 -0400

    script fixes

--- ci-dist-source.ps1
+++ ci-dist-source.ps1
@@ -1,10 +1,10 @@
 ﻿$distDir = $env:APPVEYOR_BUILD_FOLDER + "\dist\"
 $distSrcDir = $distDir + "Source\"
 $repoUrl = "https://github.com/" + $env:APPVEYOR_REPO_NAME + ".git"
-git clone -q --branch=master $repoUrl $distSrcDir
+git clone $repoUrl $distSrcDir
 cd $distSrcDir
-git checkout -qf $env:APPVEYOR_REPO_COMMIT
+git checkout $env:APPVEYOR_REPO_COMMIT
 git submodule update --init --recursive
 cd $distSrcDir
-7z a browser-source-$env:APPVEYOR_BUILD_NUMBER.zip $distSrcDir
+7z a browser-source-$env:APPVEYOR_BUILD_NUMBER.zip $distDir
 cd $env:APPVEYOR_BUILD_FOLDER
