655edc88f806ef821ca8c3b2d5413776202838d5
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-11 15:16:35 -0400

    implement getLocalesJson

--- src/services/i18n.service.ts
+++ src/services/i18n.service.ts
@@ -1,45 +1,17 @@
-import { I18nService as I18nServiceAbstraction } from 'jslib/abstractions/i18n.service';
+import { I18nService as BaseI18nService } from 'jslib/services/i18n.service';
 
-// First locale is the default (English)
-const SupportedTranslationLocales = [
-    'en', 'cs', 'da', 'de', 'es', 'et', 'fi', 'fr', 'hr', 'hu', 'id', 'it', 'ja',
-    'nb', 'nl', 'pl', 'pt-BR', 'pt-PT', 'ro', 'ru', 'sk', 'sv', 'tr', 'uk', 'vi',
-    'zh-CN', 'zh-TW',
-];
+export default class I18nService extends BaseI18nService {
+    constructor(systemLanguage: string, localesDirectory: string) {
+        super(systemLanguage, localesDirectory, async (formattedLocale: string) => {
+            const file = await fetch(localesDirectory + formattedLocale + '/messages.json');
+            return await file.json();
+        });
 
-export default class I18nService implements I18nServiceAbstraction {
-    defaultMessages: any = {};
-    localeMessages: any = {};
-    locale: string;
-    translationLocale: string;
-    collator: Intl.Collator;
-    inited: boolean;
-
-    constructor(private systemLanguage: string, private localesDirectory: string) { }
-
-    async init(locale?: string) {
-        if (this.inited) {
-            throw new Error('i18n already initialized.');
-        }
-
-        this.inited = true;
-        this.locale = this.translationLocale = locale != null ? locale : this.systemLanguage;
-        this.collator = new Intl.Collator(this.locale);
-
-        if (SupportedTranslationLocales.indexOf(this.translationLocale) === -1) {
-            this.translationLocale = this.translationLocale.slice(0, 2);
-
-            if (SupportedTranslationLocales.indexOf(this.translationLocale) === -1) {
-                this.translationLocale = SupportedTranslationLocales[0];
-            }
-        }
-
-        if (this.localesDirectory != null) {
-            await this.loadMessages(this.localesDirectory, this.translationLocale, this.localeMessages);
-            if (this.translationLocale !== SupportedTranslationLocales[0]) {
-                await this.loadMessages(this.localesDirectory, SupportedTranslationLocales[0], this.defaultMessages);
-            }
-        }
+        this.supportedTranslationLocales = [
+            'en', 'cs', 'da', 'de', 'es', 'et', 'fi', 'fr', 'hr', 'hu', 'id', 'it', 'ja',
+            'nb', 'nl', 'pl', 'pt-BR', 'pt-PT', 'ro', 'ru', 'sk', 'sv', 'tr', 'uk', 'vi',
+            'zh-CN', 'zh-TW',
+        ];
     }
 
     t(id: string, p1?: string, p2?: string, p3?: string): string {
@@ -66,56 +38,6 @@ export default class I18nService implements I18nServiceAbstraction {
             }
         }
 
-        let result: string;
-        if (this.localeMessages.hasOwnProperty(id) && this.localeMessages[id]) {
-            result = this.localeMessages[id];
-        } else if (this.defaultMessages.hasOwnProperty(id) && this.defaultMessages[id]) {
-            result = this.defaultMessages[id];
-        } else {
-            result = '';
-        }
-
-        if (result !== '') {
-            if (p1 != null) {
-                result = result.split('__$1__').join(p1);
-            }
-            if (p2 != null) {
-                result = result.split('__$2__').join(p2);
-            }
-            if (p3 != null) {
-                result = result.split('__$3__').join(p3);
-            }
-        }
-
-        return result;
+        return super.translate(id, p1, p2, p3);
     }
-
-    private async loadMessages(localesDir: string, locale: string, messagesObj: any): Promise<any> {
-        const formattedLocale = locale.replace('-', '_');
-        const file = await fetch(localesDir + formattedLocale + '/messages.json');
-        const locales = await file.json();
-        for (const prop in locales) {
-            if (!locales.hasOwnProperty(prop)) {
-                continue;
-            }
-            messagesObj[prop] = locales[prop].message;
-
-            if (locales[prop].placeholders) {
-                for (const placeProp in locales[prop].placeholders) {
-                    if (!locales[prop].placeholders.hasOwnProperty(placeProp) ||
-                        !locales[prop].placeholders[placeProp].content) {
-                        continue;
-                    }
-
-                    const replaceToken = '\\$' + placeProp.toUpperCase() + '\\$';
-                    let replaceContent = locales[prop].placeholders[placeProp].content;
-                    if (replaceContent === '$1' || replaceContent === '$2' || replaceContent === '$3') {
-                        replaceContent = '__' + replaceContent + '__';
-                    }
-                    messagesObj[prop] = messagesObj[prop].replace(new RegExp(replaceToken, 'g'), replaceContent);
-                }
-            }
-        }
-    }
-
 }
