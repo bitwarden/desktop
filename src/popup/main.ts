b88efe3e044e3681a97dea79f8dda903bf8de915
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-11 10:57:34 -0400

    polyfill web animations

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'web-animations-js';
 
 // tslint:disable-next-line
 require('./scss/popup.scss');
