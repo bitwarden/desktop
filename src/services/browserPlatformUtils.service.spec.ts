c07d2739dd8b706f0039c316a8d1f098c5141c70
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-14 10:16:05 -0400

    fix safari test

--- src/services/browserPlatformUtils.service.spec.ts
+++ src/services/browserPlatformUtils.service.spec.ts
@@ -88,6 +88,11 @@ describe('Browser Utils Service', () => {
                 value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8'
             });
 
+            Object.defineProperty(window, 'safari', {
+                configurable: true,
+                value: {}
+            });
+
             const browserPlatformUtilsService = new BrowserPlatformUtilsService(null);
             expect(browserPlatformUtilsService.getDevice()).toBe(DeviceType.Safari);
         });
