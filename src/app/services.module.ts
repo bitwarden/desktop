b89a620f9bfc0eeeeebaabf4846d24bf5230feab
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-05-30 16:52:55 -0400

    switch back to web crypto on desktop

--- src/app/services.module.ts
+++ src/app/services.module.ts
@@ -33,7 +33,6 @@ import { EnvironmentService } from 'jslib/services/environment.service';
 import { ExportService } from 'jslib/services/export.service';
 import { FolderService } from 'jslib/services/folder.service';
 import { LockService } from 'jslib/services/lock.service';
-import { NodeCryptoFunctionService } from 'jslib/services/nodeCryptoFunction.service';
 import { PasswordGenerationService } from 'jslib/services/passwordGeneration.service';
 import { SettingsService } from 'jslib/services/settings.service';
 import { StateService } from 'jslib/services/state.service';
@@ -41,6 +40,7 @@ import { SyncService } from 'jslib/services/sync.service';
 import { TokenService } from 'jslib/services/token.service';
 import { TotpService } from 'jslib/services/totp.service';
 import { UserService } from 'jslib/services/user.service';
+import { WebCryptoFunctionService } from 'jslib/services/webCryptoFunction.service';
 
 import { ApiService as ApiServiceAbstraction } from 'jslib/abstractions/api.service';
 import { AppIdService as AppIdServiceAbstraction } from 'jslib/abstractions/appId.service';
@@ -77,7 +77,8 @@ const broadcasterService = new BroadcasterService();
 const messagingService = new ElectronRendererMessagingService(broadcasterService);
 const storageService: StorageServiceAbstraction = new ElectronStorageService();
 const secureStorageService: StorageServiceAbstraction = new ElectronRendererSecureStorageService();
-const cryptoFunctionService: CryptoFunctionServiceAbstraction = new NodeCryptoFunctionService();
+const cryptoFunctionService: CryptoFunctionServiceAbstraction = new WebCryptoFunctionService(window,
+    platformUtilsService);
 const cryptoService = new CryptoService(storageService, secureStorageService, cryptoFunctionService);
 const tokenService = new TokenService(storageService);
 const appIdService = new AppIdService(storageService);
