902d6de6db06f41a50c0f5949cd456defb9a5262
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-06-04 15:24:37 -0400

    scripts for cask and snap updates

---
+++ scripts/cask-update.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [string] $version = ""
+)
+
+# Dependencies:
+# 1. brew cask install powershell
+# 2. brew install vitorgalvao/tiny-scripts/cask-repair
+#    see https://github.com/Homebrew/homebrew-cask/blob/master/CONTRIBUTING.md#updating-a-cask
+# 3. fork of homebrew-cask repo setup.
+#    see https://github.com/caskroom/homebrew-cask/blob/master/CONTRIBUTING.md#getting-set-up-to-contribute
+# 4. Environment variables for GITHUB_USER and GITHUB_TOKEN set.
+#
+# To run:
+# sh ./cask-update.sh -version 1.3.0
+
+cask-repair bitwarden --cask-version $version --blind-submit
---
+++ scripts/snap-release.ps1
@@ -0,0 +1,9 @@
+﻿# To run:
+# ./snap-release.ps1
+
+$dir = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$rootDir = $dir + "/..";
+$distDir = $rootDir + "/dist";
+$distSnap = $distDir + "/bitwarden*.snap";
+
+snap push $distSnap --release stable
