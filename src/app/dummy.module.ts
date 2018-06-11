bbd051b7e5d3000eae2d7693fe8ce5b09a65267d
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-06-11 13:40:12 -0400

    summy module

---
+++ src/app/dummy.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { InputVerbatimDirective } from 'jslib/angular/directives/input-verbatim.directive';
+
+@NgModule({
+    imports: [],
+    declarations: [
+        InputVerbatimDirective,
+    ],
+})
+export class DummyModule {
+}
