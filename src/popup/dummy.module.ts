7b84a2e5975879ab0ec798206ead1a9802ca0928
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-07 23:57:20 -0400

    dummy module

---
+++ src/popup/dummy.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { SearchPipe } from 'jslib/angular/pipes/search.pipe';
+
+@NgModule({
+    imports: [],
+    declarations: [
+        SearchPipe,
+    ],
+})
+export class DummyModule {
+}
