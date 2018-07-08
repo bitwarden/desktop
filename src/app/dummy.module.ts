d5d13c7947481c4b698009e16b77b5b2eac621d7
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-07 23:57:14 -0400

    dummy module update

--- src/app/dummy.module.ts
+++ src/app/dummy.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { InputVerbatimDirective } from 'jslib/angular/directives/input-verbatim.directive';
+import { SearchPipe } from 'jslib/angular/pipes/search.pipe';
 
 @NgModule({
     imports: [],
     declarations: [
         InputVerbatimDirective,
+        SearchPipe,
     ],
 })
 export class DummyModule {
