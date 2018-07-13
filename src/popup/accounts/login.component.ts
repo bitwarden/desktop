3fc7cf4199525dd00f569eb87a4a5e29dd760034
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-13 09:29:08 -0400

    remember email on login

--- src/popup/accounts/login.component.ts
+++ src/popup/accounts/login.component.ts
@@ -6,6 +6,7 @@ import { Angulartics2 } from 'angulartics2';
 
 import { AuthService } from 'jslib/abstractions/auth.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
+import { StorageService } from 'jslib/abstractions/storage.service';
 import { SyncService } from 'jslib/abstractions/sync.service';
 
 import { LoginComponent as BaseLoginComponent } from 'jslib/angular/components/login.component';
@@ -17,8 +18,8 @@ import { LoginComponent as BaseLoginComponent } from 'jslib/angular/components/l
 export class LoginComponent extends BaseLoginComponent {
     constructor(authService: AuthService, router: Router,
         analytics: Angulartics2, toasterService: ToasterService,
-        i18nService: I18nService, private syncService: SyncService) {
-        super(authService, router, analytics, toasterService, i18nService);
+        i18nService: I18nService, private syncService: SyncService, storageService: StorageService) {
+        super(authService, router, analytics, toasterService, i18nService, storageService);
         super.onSuccessfullLogin = () => {
             return syncService.fullSync(true);
         };
