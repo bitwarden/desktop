273e9bb79d117b63a39bc7211ddb4ef3edbde4fc
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-26 21:53:35 -0400

    choco update script downloads exe from github

--- scripts/choco-update.ps1
+++ scripts/choco-update.ps1
@@ -1,5 +1,10 @@
-﻿# To run:
-# .\choco-update.ps1
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string] $version
+)
+
+# To run:
+# .\choco-update.ps1 -version 1.3.0
 
 $dir = Split-Path -Parent $MyInvocation.MyCommand.Path;
 $rootDir = $dir + "\..";
@@ -13,16 +18,17 @@ if(Test-Path -Path $distChocoDir) {
 
 Copy-Item -Path $chocoDir -Destination $distChocoDir –Recurse
 
-$srcPackage = $rootDir + "\src\package.json";
-$srcPackageVersion = (Get-Content -Raw -Path $srcPackage | ConvertFrom-Json).version;
+$exe = $distChocoDir + "\Bitwarden-Installer-" + $version + ".exe";
+$uri = "https://github.com/bitwarden/desktop/releases/download/v" + $version + "/Bitwarden-Installer-" + $version + ".exe";
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod -Uri $uri -OutFile $exe
 
-$exe = $distDir + "\nsis-web\Bitwarden-Installer-" + $srcPackageVersion + ".exe";
 $checksum = checksum -t sha256 $exe
 $nuspec = $distChocoDir + "\bitwarden.nuspec";
 $chocoInstall = $distChocoDir + "\tools\chocolateyinstall.ps1";
 
-(Get-Content $chocoInstall).replace('__version__', $srcPackageVersion).replace('__checksum__', $checksum) | Set-Content $chocoInstall
-choco pack $nuspec --version $srcPackageVersion --out $distChocoDir
+(Get-Content $chocoInstall).replace('__version__', $version).replace('__checksum__', $checksum) | Set-Content $chocoInstall
+choco pack $nuspec --version $version --out $distChocoDir
 cd $distChocoDir
 choco push
 cd $rootDir
