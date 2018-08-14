88abba637e2292c87fbd405ada56e5676eb9547a
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-08-14 15:12:32 -0400

    export deps adjusted

--- src/app/vault/export.component.ts
+++ src/app/vault/export.component.ts
@@ -7,7 +7,6 @@ import { CryptoService } from 'jslib/abstractions/crypto.service';
 import { ExportService } from 'jslib/abstractions/export.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
-import { UserService } from 'jslib/abstractions/user.service';
 
 import { ExportComponent as BaseExportComponent } from 'jslib/angular/components/export.component';
 
@@ -17,10 +16,9 @@ import { ExportComponent as BaseExportComponent } from 'jslib/angular/components
 })
 export class ExportComponent extends BaseExportComponent {
     constructor(analytics: Angulartics2, toasterService: ToasterService,
-        cryptoService: CryptoService, userService: UserService,
-        i18nService: I18nService, platformUtilsService: PlatformUtilsService,
-        exportService: ExportService) {
-        super(analytics, toasterService, cryptoService, userService, i18nService, platformUtilsService,
+        cryptoService: CryptoService, i18nService: I18nService,
+        platformUtilsService: PlatformUtilsService, exportService: ExportService) {
+        super(analytics, toasterService, cryptoService, i18nService, platformUtilsService,
             exportService, window);
     }
 }
