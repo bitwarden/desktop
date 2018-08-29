8f92e478ba3475d2f8c85c2704b0cb329e808178
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-08-29 09:31:25 -0400

    fix attachments deps

--- src/app/vault/attachments.component.ts
+++ src/app/vault/attachments.component.ts
@@ -7,7 +7,7 @@ import { CipherService } from 'jslib/abstractions/cipher.service';
 import { CryptoService } from 'jslib/abstractions/crypto.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
-import { TokenService } from 'jslib/abstractions/token.service';
+import { UserService } from 'jslib/abstractions/user.service';
 
 import { AttachmentsComponent as BaseAttachmentsComponent } from 'jslib/angular/components/attachments.component';
 
@@ -18,9 +18,9 @@ import { AttachmentsComponent as BaseAttachmentsComponent } from 'jslib/angular/
 export class AttachmentsComponent extends BaseAttachmentsComponent {
     constructor(cipherService: CipherService, analytics: Angulartics2,
         toasterService: ToasterService, i18nService: I18nService,
-        cryptoService: CryptoService, tokenService: TokenService,
+        cryptoService: CryptoService, userService: UserService,
         platformUtilsService: PlatformUtilsService) {
-        super(cipherService, analytics, toasterService, i18nService, cryptoService, tokenService,
+        super(cipherService, analytics, toasterService, i18nService, cryptoService, userService,
             platformUtilsService, window);
     }
 }
