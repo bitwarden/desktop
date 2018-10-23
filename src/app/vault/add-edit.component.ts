2244519596e1b35a08aad5809becc8b47318a541
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-10-23 13:35:26 -0400

    fix add/edit

--- src/app/vault/add-edit.component.ts
+++ src/app/vault/add-edit.component.ts
@@ -5,10 +5,12 @@ import {
 
 import { AuditService } from 'jslib/abstractions/audit.service';
 import { CipherService } from 'jslib/abstractions/cipher.service';
+import { CollectionService } from 'jslib/abstractions/collection.service';
 import { FolderService } from 'jslib/abstractions/folder.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 import { StateService } from 'jslib/abstractions/state.service';
+import { UserService } from 'jslib/abstractions/user.service';
 
 import { AddEditComponent as BaseAddEditComponent } from 'jslib/angular/components/add-edit.component';
 
@@ -19,8 +21,10 @@ import { AddEditComponent as BaseAddEditComponent } from 'jslib/angular/componen
 export class AddEditComponent extends BaseAddEditComponent implements OnChanges {
     constructor(cipherService: CipherService, folderService: FolderService,
         i18nService: I18nService, platformUtilsService: PlatformUtilsService,
-        auditService: AuditService, stateService: StateService) {
-        super(cipherService, folderService, i18nService, platformUtilsService, auditService, stateService);
+        auditService: AuditService, stateService: StateService,
+        userService: UserService, collectionService: CollectionService) {
+        super(cipherService, folderService, i18nService, platformUtilsService, auditService, stateService,
+            userService, collectionService);
     }
 
     async ngOnChanges() {
