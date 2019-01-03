06736b97d13cdef1f047857ed90e3b52fa638569
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-03 10:23:45 -0500

    add new messaging service dependecy

--- src/app/vault/add-edit.component.ts
+++ src/app/vault/add-edit.component.ts
@@ -8,6 +8,7 @@ import { CipherService } from 'jslib/abstractions/cipher.service';
 import { CollectionService } from 'jslib/abstractions/collection.service';
 import { FolderService } from 'jslib/abstractions/folder.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
+import { MessagingService } from 'jslib/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 import { StateService } from 'jslib/abstractions/state.service';
 import { UserService } from 'jslib/abstractions/user.service';
@@ -22,9 +23,10 @@ export class AddEditComponent extends BaseAddEditComponent implements OnChanges 
     constructor(cipherService: CipherService, folderService: FolderService,
         i18nService: I18nService, platformUtilsService: PlatformUtilsService,
         auditService: AuditService, stateService: StateService,
-        userService: UserService, collectionService: CollectionService) {
+        userService: UserService, collectionService: CollectionService,
+        messagingService: MessagingService) {
         super(cipherService, folderService, i18nService, platformUtilsService, auditService, stateService,
-            userService, collectionService);
+            userService, collectionService, messagingService);
     }
 
     async ngOnInit() {
