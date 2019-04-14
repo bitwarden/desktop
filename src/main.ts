5f4a5faf3c46a9c1d4d936966e886312c9e24be3
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-13 21:37:42 -0400

    hideTitleBar true

--- src/main.ts
+++ src/main.ts
@@ -64,7 +64,7 @@ export class Main {
         storageDefaults[ConstantsService.lockOptionKey] = -1;
         this.storageService = new ElectronStorageService(app.getPath('userData'), storageDefaults);
 
-        this.windowMain = new WindowMain(this.storageService);
+        this.windowMain = new WindowMain(this.storageService, true);
         this.messagingMain = new MessagingMain(this);
         this.updaterMain = new UpdaterMain(this.i18nService, this.windowMain, 'desktop', () => {
             this.menuMain.updateMenuItem.enabled = false;
