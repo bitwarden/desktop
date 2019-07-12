d90b61c225430d8dbdfddcca3337452d8453d905
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-07-12 17:11:39 -0400

    export vault event

--- src/app/vault/export.component.ts
+++ src/app/vault/export.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { CryptoService } from 'jslib/abstractions/crypto.service';
+import { EventService } from 'jslib/abstractions/event.service';
 import { ExportService } from 'jslib/abstractions/export.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
@@ -13,7 +14,8 @@ import { ExportComponent as BaseExportComponent } from 'jslib/angular/components
 })
 export class ExportComponent extends BaseExportComponent {
     constructor(cryptoService: CryptoService, i18nService: I18nService,
-        platformUtilsService: PlatformUtilsService, exportService: ExportService) {
-        super(cryptoService, i18nService, platformUtilsService, exportService, window);
+        platformUtilsService: PlatformUtilsService, exportService: ExportService,
+        eventService: EventService) {
+        super(cryptoService, i18nService, platformUtilsService, exportService, eventService, window);
     }
 }
