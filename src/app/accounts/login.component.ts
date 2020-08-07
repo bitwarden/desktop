534c6a4310eedd1b8435ef6438b691319a34c0dc
Vincent Salucci <vincesalucci21@gmail.com>
2020-08-06 22:26:19 -0500

    First round of UI updates // Added sso browser launching // Added missing strings

--- src/app/accounts/login.component.ts
+++ src/app/accounts/login.component.ts
@@ -4,12 +4,16 @@ import {
     ViewChild,
     ViewContainerRef,
 } from '@angular/core';
+
 import { Router } from '@angular/router';
 
 import { EnvironmentComponent } from './environment.component';
 
 import { AuthService } from 'jslib/abstractions/auth.service';
+import { CryptoFunctionService } from 'jslib/abstractions/cryptoFunction.service';
+import { EnvironmentService } from 'jslib/abstractions/environment.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
+import { PasswordGenerationService } from 'jslib/abstractions/passwordGeneration.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 import { StateService } from 'jslib/abstractions/state.service';
 import { StorageService } from 'jslib/abstractions/storage.service';
@@ -17,6 +21,8 @@ import { SyncService } from 'jslib/abstractions/sync.service';
 
 import { LoginComponent as BaseLoginComponent } from 'jslib/angular/components/login.component';
 import { ModalComponent } from 'jslib/angular/components/modal.component';
+
+import { Utils } from 'jslib/misc/utils';
 
 @Component({
     selector: 'app-login',
@@ -30,7 +36,9 @@ export class LoginComponent extends BaseLoginComponent {
     constructor(authService: AuthService, router: Router,
         i18nService: I18nService, syncService: SyncService,
         private componentFactoryResolver: ComponentFactoryResolver, storageService: StorageService,
-        platformUtilsService: PlatformUtilsService, stateService: StateService) {
+        platformUtilsService: PlatformUtilsService, stateService: StateService,
+        private environmentService: EnvironmentService, private passwordGenerationService: PasswordGenerationService,
+        private cryptoFunctionService: CryptoFunctionService) {
         super(authService, router, platformUtilsService, i18nService, storageService, stateService);
         super.onSuccessfulLogin = () => {
             return syncService.fullSync(true);
@@ -54,5 +62,32 @@ export class LoginComponent extends BaseLoginComponent {
         childComponent.onSaved.subscribe(() => {
             modal.close();
         });
+    }
+
+    async launchSsoBrowser() {
+        // Generate necessary sso params
+        const passwordOptions: any = {
+            type: 'password',
+            length: 64,
+            uppercase: true,
+            lowercase: true,
+            numbers: true,
+            special: false,
+        };
+        const state = await this.passwordGenerationService.generatePassword(passwordOptions);
+        let ssoCodeVerifier = await this.passwordGenerationService.generatePassword(passwordOptions);
+        const codeVerifierHash = await this.cryptoFunctionService.hash(ssoCodeVerifier, 'sha256');
+        const codeChallenge = Utils.fromBufferToUrlB64(codeVerifierHash);
+
+        // Build URI
+        const webUrl = this.environmentService.webVaultUrl == null ? 'https://vault.bitwarden.com' :
+            this.environmentService.webVaultUrl;
+        const clientId = 'desktop';
+        const ssoRedirectUri = 'bitwarden://sso-callback';
+
+        // Launch browser
+        this.platformUtilsService.launchUri(webUrl + '/#/sso?clientId=' + clientId +
+            '&redirectUri=' + encodeURIComponent(ssoRedirectUri) +
+            '&state=' + state + '&codeChallenge=' + codeChallenge);
     }
 }
