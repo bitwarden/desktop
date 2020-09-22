0a310b5a002490b10937bf34788b68239d9fd9f3
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-09-22 16:32:39 -0400

    add apple id info

--- .github/scripts/macos/setup-keychain.ps1
+++ .github/scripts/macos/setup-keychain.ps1
@@ -11,6 +11,7 @@ security create-keychain -p $env:KEYCHAIN_PASSWORD build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p $env:KEYCHAIN_PASSWORD build.keychain
 security set-keychain-settings -lut 1200 build.keychain
+security add-generic-password -a "AC_USERNAME" -w $env:APPLE_ID_PASSWORD -s "AC_PASSWORD"
 security import $desktopKeyPath -k build.keychain -P $env:DESKTOP_KEY_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security import $devidAppCertPath -k build.keychain -P $env:DEVID_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security import $devidInstallerCertPath -k build.keychain -P $env:DEVID_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
