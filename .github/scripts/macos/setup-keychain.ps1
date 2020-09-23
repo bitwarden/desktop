e2da97ec7889c221beba93490c0dacb0215eb33d
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-09-23 13:59:37 -0400

    macdev cert

--- .github/scripts/macos/setup-keychain.ps1
+++ .github/scripts/macos/setup-keychain.ps1
@@ -6,6 +6,7 @@ $devidAppCertPath = $($secretsPath + "/devid-app-cert.p12");
 $devidInstallerCertPath = $($secretsPath + "/devid-installer-cert.p12");
 $appstoreAppCertPath = $($secretsPath + "/appstore-app-cert.p12");
 $appstoreInstallerCertPath = $($secretsPath + "/appstore-installer-cert.p12");
+$macdevCertPath = $($secretsPath + "/macdev-cert.p12");
 
 security create-keychain -p $env:KEYCHAIN_PASSWORD build.keychain
 security default-keychain -s build.keychain
@@ -16,4 +17,5 @@ security import $devidAppCertPath -k build.keychain -P $env:DEVID_CERT_PASSWORD 
 security import $devidInstallerCertPath -k build.keychain -P $env:DEVID_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security import $appstoreAppCertPath -k build.keychain -P $env:APPSTORE_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security import $appstoreInstallerCertPath -k build.keychain -P $env:APPSTORE_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
+security import $macdevCertPath -k build.keychain -P $env:MACDEV_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $env:KEYCHAIN_PASSWORD build.keychain
