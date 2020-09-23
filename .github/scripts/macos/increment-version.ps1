2eb60e95aea5925f887c4a7b528c43b28f24c6f2
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-09-23 12:44:57 -0400

    increment version

--- .github/scripts/macos/increment-version.ps1
+++ .github/scripts/macos/increment-version.ps1
@@ -1,2 +1,5 @@
 ﻿$rootPath = $env:GITHUB_WORKSPACE;
 $buildNumber = 100 + [int]$env:GITHUB_RUN_NUMBER;
+# See https://www.electron.build/configuration/configuration#configuration
+Write-Output "Setting build number to $buildNumber";
+Write-Output "::set-env name=BUILD_NUMBER::$buildNumber";
