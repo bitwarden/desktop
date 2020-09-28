9f357cb2acc8fc1fe24bdafbb78b5005b39e5fd5
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-09-28 11:11:34 -0400

    increment build.buildVersion

--- .github/scripts/macos/increment-version.ps1
+++ .github/scripts/macos/increment-version.ps1
@@ -1,5 +1,7 @@
 ﻿$rootPath = $env:GITHUB_WORKSPACE;
+$packagePath = "$rootPath\package.json";
 $buildNumber = 100 + [int]$env:GITHUB_RUN_NUMBER;
-# See https://www.electron.build/configuration/configuration#configuration
 Write-Output "Setting build number to $buildNumber";
-Write-Output "::set-env name=BUILD_NUMBER::$buildNumber";
+$package = Get-Content -Raw -Path $packagePath | ConvertFrom-Json;
+$package.build | Add-Member -MemberType NoteProperty -Name buildVersion -Value "$buildNumber";
+$package | ConvertTo-Json -Depth 32 | Set-Content $packagePath;
