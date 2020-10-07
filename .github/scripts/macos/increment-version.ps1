d95fc33f2f2c584c31086afaf6bc0bd5351defe6
ummekulsum123 <72161112+ummekulsum123@users.noreply.github.com>
2020-10-07 05:15:57 -0700

    Update increment-version.ps1

--- .github/scripts/macos/increment-version.ps1
+++ .github/scripts/macos/increment-version.ps1
@@ -1,8 +1,10 @@
 ﻿$rootPath = $env:GITHUB_WORKSPACE;
 $packagePath = "$rootPath\package.json";
 $buildNumber = 100 + [int]$env:GITHUB_RUN_NUMBER;
+
 Write-Output "Setting build number to $buildNumber";
 Write-Output "::set-env name=BUILD_NUMBER::$buildNumber";
+
 $package = Get-Content -Raw -Path $packagePath | ConvertFrom-Json;
 $package.build | Add-Member -MemberType NoteProperty -Name buildVersion -Value "$buildNumber";
 $package | ConvertTo-Json -Depth 32 | Set-Content $packagePath;
