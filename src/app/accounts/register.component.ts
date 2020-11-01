0289fc25eeb0167143c7ac74ddf3fb8ffb82ab57
Vincent Salucci <vincesalucci21@gmail.com>
2020-11-01 13:53:00 -0600

    initial commit gdpr acceptance

--- src/app/accounts/register.component.ts
+++ src/app/accounts/register.component.ts
@@ -16,11 +16,25 @@ import { RegisterComponent as BaseRegisterComponent } from 'jslib/angular/compon
     templateUrl: 'register.component.html',
 })
 export class RegisterComponent extends BaseRegisterComponent {
+    showTerms = true;
+    acceptPolicies: boolean = false;
+
     constructor(authService: AuthService, router: Router,
         i18nService: I18nService, cryptoService: CryptoService,
         apiService: ApiService, stateService: StateService,
         platformUtilsService: PlatformUtilsService, passwordGenerationService: PasswordGenerationService) {
         super(authService, router, i18nService, cryptoService, apiService, stateService, platformUtilsService,
             passwordGenerationService);
+        this.showTerms = !platformUtilsService.isSelfHost();
+    }
+
+    async submit() {
+        if (!this.acceptPolicies && this.showTerms) {
+            this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
+                this.i18nService.t('acceptPoliciesError'));
+            return;
+        }
+
+        await super.submit();
     }
 }
