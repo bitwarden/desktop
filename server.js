f0be753dc959304e933017d44879005b0670a19a
Ben <mail1@sonadresse.com>
2020-12-20 16:03:28 +0100

    npm run electron:browser - OK

---
+++ server.js
@@ -0,0 +1,32 @@
+/* ===============================================================================
+
+Expose en local des pages web avec des cas de tests de formulaires pour Cozy Pass
+url: http://localhost:3333/
+
+=================================================================================== */
+
+var http = require('http')
+var fs   = require('fs'  )
+
+http.createServer(function (req, res){
+
+    var filePath = '.' + req.url
+    if (filePath === './') {
+        filePath = 'index.html'
+    }
+
+    if (!fs.existsSync(filePath)) {
+        console.log("file doesnt exist", filePath);
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.write('404 Not Found\n');
+        res.end();
+        return
+    }
+
+    console.log("request of file :", filePath);
+    // res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(fs.readFileSync(filePath))
+
+}).listen(4242);
+
+console.log('server listening http://localhost:4242/');
---
+++ webpack.browser.js
@@ -0,0 +1,144 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
+
+const common = {
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+            },
+            {
+                test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+                loader: '@ngtools/webpack',
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                exclude: /.*(fontawesome-webfont)\.svg/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images/',
+                    },
+                }],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin([
+            path.resolve(__dirname, 'build-web/*'),
+        ]),
+        new CopyWebpackPlugin([
+            './src/package.json',
+            { from: './src/images', to: 'images' },
+            { from: './src/locales', to: 'locales' },
+            { from: './server.js', to: 'server.js' },
+        ]),
+        new webpack.EnvironmentPlugin(['ELECTRON_IS_DEV', 'ENV']),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            jslib: path.join(__dirname, 'jslib/src'),
+        },
+        symlinks: false,
+        modules: [path.resolve('node_modules')],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'build'),
+    },
+};
+
+const renderer = {
+    mode: 'production',
+    devtool: false,
+    target: 'electron-renderer',
+    node: {
+        __dirname: false,
+    },
+    entry: {
+        'app/main': './src/app/main.ts',
+    },
+    optimization: {
+        minimize: false,
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'app/vendor',
+                    chunks: (chunk) => {
+                        return chunk.name === 'app/main';
+                    },
+                },
+            },
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                exclude: /loading.svg/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    },
+                }],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            // Hide System.import warnings. ref: https://github.com/angular/angular/issues/21560
+            {
+                test: /[\/\\]@angular[\/\\].+\.js$/,
+                parser: { system: true },
+            },
+        ],
+    },
+    plugins: [
+        new AngularCompilerPlugin({
+            tsConfigPath: 'tsconfig.json',
+            entryModule: 'src/app/app.module#AppModule',
+            sourceMap: true,
+        }),
+        // ref: https://github.com/angular/angular/issues/20357
+        new webpack.ContextReplacementPlugin(/\@angular(\\|\/)core(\\|\/)fesm5/,
+            path.resolve(__dirname, './src')),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            chunks: ['app/vendor', 'app/main'],
+        }),
+        new webpack.SourceMapDevToolPlugin({
+            include: ['app/main.js'],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+        }),
+    ],
+};
+
+module.exports = merge(common, renderer);
