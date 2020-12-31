f41014f0f2f8633e518ab3013f7f4da819ee851f
Joseph Flinn <joseph.s.flinn@gmail.com>
2020-12-31 17:33:33 +0000

    trying pipeline env to pass vars

--- sign.js
+++ sign.js
@@ -1,9 +1,9 @@
 exports.default = async function(configuration) {
   //console.log(`config:\n${JSON.stringify(configuration, null, 4)}`)
-  console.log(`env test - secret test ${process.env.secretTest}`)
+  console.log(`env test - secret test ${process.env.SECRET_TEST}`)
 
   require("child_process").execSync(
-    `azuresigntool sign -kvu ${process.env.SigningVaultURL} -kvi ${process.env.SigningClientId} -kvs ${process.env.SigningClientSecret} -kvc ${process.env.SigningCertName} -fd ${configuration.hash}-du ${configuration.site} -tr http://timestamp.digicert.com ${configuration.path}`,
+    `azuresigntool sign -kvu ${process.env.SIGNING_VAULT_URL} -kvi ${process.env.SIGNING_CLIENT_ID} -kvs ${process.env.SIGNING_CLIENT_SECRET} -kvc ${process.env.SIGNING_CERT_NAME} -fd ${configuration.hash} -du ${configuration.site} -tr http://timestamp.digicert.com ${configuration.path}`,
     {
       stdio: "inherit"
     }
