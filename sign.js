711cea8c4b5cfc241537ab2e9cc088afb1925bad
Joseph Flinn <joseph.s.flinn@gmail.com>
2020-12-31 22:09:00 +0000

    Install and use latest AST (this will probably break when it is updated because Google won't tell me how to make a clean build of a .NET project)

--- sign.js
+++ sign.js
@@ -3,7 +3,7 @@ exports.default = async function(configuration) {
   console.log(`env test - secret test ${process.env.SECRET_TEST}`)
 
   require("child_process").execSync(
-    `azuresigntool sign -kvu ${process.env.SIGNING_VAULT_URL} -kvi ${process.env.SIGNING_CLIENT_ID} -kvs ${process.env.SIGNING_CLIENT_SECRET} -kvc ${process.env.SIGNING_CERT_NAME} -fd ${configuration.hash} -du ${configuration.site} -tr http://timestamp.digicert.com ${configuration.path}`,
+    `azuresigntool sign -kvu ${process.env.SIGNING_VAULT_URL} -kvi ${process.env.SIGNING_CLIENT_ID} -kvt ${process.env.SIGNING_TENANT_ID} -kvs ${process.env.SIGNING_CLIENT_SECRET} -kvc ${process.env.SIGNING_CERT_NAME} -fd ${configuration.hash} -du ${configuration.site} -tr http://timestamp.digicert.com ${configuration.path}`,
     {
       stdio: "inherit"
     }
