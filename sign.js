9581af66a1a3a69313cd3f82a8a3595277577c0b
Joseph Flinn <joseph.s.flinn@gmail.com>
2020-12-31 17:07:14 +0000

    testing secret in process.env

--- sign.js
+++ sign.js
@@ -1,17 +1,9 @@
 exports.default = async function(configuration) {
   //console.log(`config:\n${JSON.stringify(configuration, null, 4)}`)
-  console.log(`env test - vault URL: ${process.env.SigningVaultURL}`)
+  console.log(`env test - secret test ${process.env.secretTest}`)
 
   require("child_process").execSync(
-    `azuresigntool sign \
-    -kvu ${process.env.SigningVaultURL} \
-    -kvi ${process.env.SigningClientId} \ 
-    -kvs ${process.env.SigningClientSecret} \
-    -kvc ${process.env.SigningCertName} \
-    -fd ${configuration.hash}
-    -du ${configuration.site}
-    -tr http://timestamp.digicert.com \
-    ${configuration.path}`,
+    `azuresigntool sign -kvu ${process.env.SigningVaultURL} -kvi ${process.env.SigningClientId} -kvs ${process.env.SigningClientSecret} -kvc ${process.env.SigningCertName} -fd ${configuration.hash}-du ${configuration.site} -tr http://timestamp.digicert.com ${configuration.path}`,
     {
       stdio: "inherit"
     }
