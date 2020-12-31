d4f6c07fc079b6595b68c4954d7a6a9f49c311e1
Joseph Flinn <joseph.s.flinn@gmail.com>
2020-12-31 16:28:44 +0000

    testing the env vars access

--- sign.js
+++ sign.js
@@ -1,12 +1,15 @@
 exports.default = async function(configuration) {
-  console.log(`config:\n${JSON.stringify(configuration, null, 4)}`)
+  //console.log(`config:\n${JSON.stringify(configuration, null, 4)}`)
+  console.log(`env test - vault URL: ${process.env.SigningVaultURL}`)
 
   require("child_process").execSync(
     `azuresigntool sign \
-    -kvu "${process.env.SigningVaultURL}" \
-    -kvi "${process.env.SigningClientId}" \ 
-    -kvs "${process.env.SigningClientSecret}" \
-    -kvc "${process.env.SigningCertName}" \
+    -kvu ${process.env.SigningVaultURL} \
+    -kvi ${process.env.SigningClientId} \ 
+    -kvs ${process.env.SigningClientSecret} \
+    -kvc ${process.env.SigningCertName} \
+    -fd ${configuration.hash}
+    -du ${configuration.site}
     -tr http://timestamp.digicert.com \
     ${configuration.path}`,
     {
