190fb423da418fd1270483f8c37e5d32d082ed6d
Joseph Flinn <joseph.s.flinn@gmail.com>
2021-01-06 19:46:37 +0000

    fixing unknown syntax error

--- sign.js
+++ sign.js
@@ -1,11 +1,5 @@
 exports.default = async function(configuration) {
-
-  console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++')
-  console.log(`ELECTRON_BUILDER_SIGN: ${process.env.ELECTRON_BUILDER_SIGN}`)
-  console.log(`Type: ${typeof process.env.ELECTRON_BUILDER_SIGN}`)
-  console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++')
-
-  if (parseInt(process.env.ELECTRON_BUILDER_SIGN) === 1) {
+  if (process.env.ELECTRON_BUILDER_SIGN === 1) {
     require("child_process").execSync(
       `azuresigntool sign ` +
       `-kvu ${process.env.SIGNING_VAULT_URL} ` +
