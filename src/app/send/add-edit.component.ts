30a7db2f20cbfed8506445d731659533988fc96d
addison <addisonbeck1@gmail.com>
2021-02-09 15:57:10 -0500

    Added support for viewing Sends'

--- src/app/send/add-edit.component.ts
+++ src/app/send/add-edit.component.ts
@@ -1,11 +1,12 @@
 import { DatePipe } from '@angular/common';
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { EnvironmentService } from 'jslib/abstractions/environment.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { MessagingService } from 'jslib/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
+import { PolicyService } from 'jslib/abstractions/policy.service';
 import { SendService } from 'jslib/abstractions/send.service';
 import { UserService } from 'jslib/abstractions/user.service';
 
@@ -19,8 +20,13 @@ export class AddEditComponent extends BaseAddEditComponent {
     constructor(i18nService: I18nService, platformUtilsService: PlatformUtilsService,
         environmentService: EnvironmentService, datePipe: DatePipe,
         sendService: SendService, userService: UserService,
-        messagingService: MessagingService) {
+        messagingService: MessagingService, policyService: PolicyService) {
         super(i18nService, platformUtilsService, environmentService,
-              datePipe, sendService, userService, messagingService);
+              datePipe, sendService, userService, messagingService, policyService);
+    }
+
+    async refresh() {
+        const send = await this.loadSend();
+        this.send = await send.decrypt();
     }
 }
