1d734b687def70bce0011fc40e937b99bc60f744
Hinton <hinton.oscar@gmail.com>
2021-03-18 23:15:15 +0100

    Unzip using built in mac unzip util which respects mac magic +x flags

--- scripts/download-legacy-safari.ps1
+++ scripts/download-legacy-safari.ps1
@@ -1,10 +1,12 @@
 $dir = Split-Path -Parent $MyInvocation.MyCommand.Path;
 $rootDir = $dir + "\..";
 $outputPath = $rootDir + "\PlugIns";
-$downloadOutput = $outputPath + "\safari-legacy.appex.zip"
+$zipFileName = "safari-legacy.appex.zip";
+$downloadOutput = $outputPath + "\" + $zipFileName;
 
 Invoke-WebRequest -Uri "https://github.com/bitwarden/browser/releases/download/v1.48.1/safari-legacy.appex.zip" -OutFile $downloadOutput
 
-Expand-Archive -Path $downloadOutput -DestinationPath $outputPath
+Set-Location $outputPath
+Invoke-Expression -Command "unzip $zipFileName"
 
 Remove-Item $downloadOutput
