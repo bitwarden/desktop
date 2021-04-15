0724041dafb502e39e19bc7ae0b1b3a9ed6d203c
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2021-04-15 18:29:26 +1000

    Don't use tokenService to manage emailVerified (#841)
    * Update send add-edit component dependencies
    
    * bump jslib

--- src/app/send/add-edit.component.ts
+++ src/app/send/add-edit.component.ts
@@ -8,7 +8,6 @@ import { MessagingService } from 'jslib/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 import { PolicyService } from 'jslib/abstractions/policy.service';
 import { SendService } from 'jslib/abstractions/send.service';
-import { TokenService } from 'jslib/abstractions/token.service';
 import { UserService } from 'jslib/abstractions/user.service';
 
 import { AddEditComponent as BaseAddEditComponent } from 'jslib/angular/components/send/add-edit.component';
@@ -21,9 +20,9 @@ export class AddEditComponent extends BaseAddEditComponent {
     constructor(i18nService: I18nService, platformUtilsService: PlatformUtilsService,
         environmentService: EnvironmentService, datePipe: DatePipe,
         sendService: SendService, userService: UserService,
-        messagingService: MessagingService, policyService: PolicyService, tokenService: TokenService) {
+        messagingService: MessagingService, policyService: PolicyService) {
         super(i18nService, platformUtilsService, environmentService,
-              datePipe, sendService, userService, messagingService, policyService, tokenService);
+              datePipe, sendService, userService, messagingService, policyService);
     }
 
     async refresh() {
