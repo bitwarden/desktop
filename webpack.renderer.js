e6e79917f7f41e9221fad00da3b3d994781010f5
Oscar Hinton <oscar@oscarhinton.com>
2021-04-23 10:45:30 +0200

    Bump dependencies (#848)
    * Bump dependencies

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
