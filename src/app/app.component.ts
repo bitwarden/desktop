82c1d43b57092a046fb93725eb17a0a11b9a3c45
Yannick Chiron <yannick.chiron@gmail.com>
2021-06-14 15:12:50 +0200

    feat: Init cozy-bar and Cozy-Client
    Needs to make a PR in Cozy-Client to remove an hook depending to UI...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+/* global cozy */
+type InitialData = {
+    bar: any;
+  };
+declare var cozy: InitialData;
 import {
     BodyOutputType,
     Toast,
@@ -52,6 +57,8 @@ import { _showDialog } from 'browser/functionForTarget._showDialog';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 
 import { ConstantsService } from 'jslib/services/constants.service';
+
+import CozyClient from 'cozy-client';
 
 import { CipherType } from 'jslib/enums/cipherType';
 
@@ -127,6 +134,8 @@ export class AppComponent implements OnInit {
             window.onscroll = () => this.recordActivity();
             window.onkeypress = () => this.recordActivity();
         });
+
+        this.initCozy();
 
         this.broadcasterService.subscribe(BroadcasterSubscriptionId, async (message: any) => {
             this.ngZone.run(async () => {
@@ -281,6 +290,37 @@ export class AppComponent implements OnInit {
                 }
             });
         });
+        
+    }
+
+    initCozy() {
+        const root = document.querySelector('[role=application]');
+        if (root instanceof HTMLElement) {
+            const data = root.dataset;
+
+            const protocol = window.location ? window.location.protocol : 'https:';
+            const cozyUrl = `${protocol}//${data.cozyDomain}`;
+            const appMetadata = {
+                slug: 'password',
+                version: '1'
+            };
+
+            const client = new CozyClient({
+                uri: cozyUrl,
+                token: data.cozyToken,
+                appMetadata,
+                schema: {}
+            });
+
+            cozy.bar.init({
+                appName: data.cozyAppName,
+                appEditor: data.cozyAppEditor,
+                cozyClient: client,
+                iconPath: data.cozyIconPath,
+                lang: data.cozyLocale,
+                replaceTitleOnMobile: false
+            });
+        }
     }
 
     ngOnDestroy() {
