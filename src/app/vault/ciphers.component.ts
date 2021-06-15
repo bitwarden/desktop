3f45cc1ae50826d834c2f4a897a9c053df6cca46
Thomas Rittson <trittson@bitwarden.com>
2021-06-15 14:06:57 +1000

    add trackBy, reorder dom

--- src/app/vault/ciphers.component.ts
+++ src/app/vault/ciphers.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 import { CiphersComponent as BaseCiphersComponent } from 'jslib-angular/components/ciphers.component';
 
+import { CipherView } from 'jslib-common/models/view/cipherView';
+
 @Component({
     selector: 'app-vault-ciphers',
     templateUrl: 'ciphers.component.html',
 })
-export class CiphersComponent extends BaseCiphersComponent { }
+export class CiphersComponent extends BaseCiphersComponent { 
+    trackByFn(index: number, c: CipherView) {
+        return c.id;
+    }
+}
