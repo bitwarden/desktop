fc2a92b4b9ae43853c6ef1a8f62cb694975abe14
Yannick Chiron <yannick.chiron@gmail.com>
2021-07-08 16:42:40 +0200

    fix: remove InstalledStep and ConnectedStep from InstallationPage
    Those steps are not used anymore as user is redirected to login
    page when vault is configured

--- src/cozy/react/components/ConfigureExtensionStep.jsx
+++ src/cozy/react/components/ConfigureExtensionStep.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import {
-  useExtensionStatus,
-  extensionStatuses
+  useExtensionStatus
 } from '../helpers/extensionStatus'
 
 import InstallationStep from './InstallationStep'
-import InstalledStep from './InstalledStep'
-import ConnectedStep from './ConnectedStep'
 
 const ConfigureExtensionStep = ({ onConnected, onSkipExtension }) => {
   const extensionStatus = useExtensionStatus()
-  if (extensionStatus == extensionStatuses.installed) {
-    return <InstalledStep onConnected={onConnected} />
-  } else if (extensionStatus === extensionStatuses.connected) {
-    return <ConnectedStep />
-  } else {
-    return <InstallationStep onSkipExtension={onSkipExtension} />
-  }
+  return <InstallationStep onSkipExtension={onSkipExtension} />
 }
 
 export default ConfigureExtensionStep
