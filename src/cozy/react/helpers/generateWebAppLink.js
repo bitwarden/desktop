42715b5953f8cbb1119ce7c9e8c0cc94a03ac73f
Yannick Chiron <yannick.chiron@gmail.com>
2021-09-07 16:31:43 +0200

    fix: correctly compute the app's domain type
    Call to `client.getInstanceOptions()` was destructured using old
    variable name which resulted to an undefined value and a always `flat`
    generated link

--- src/cozy/react/helpers/generateWebAppLink.js
+++ src/cozy/react/helpers/generateWebAppLink.js
@@ -2,11 +2,11 @@ import { generateWebLink } from 'cozy-ui/transpiled/react/AppLinker'
 
 const generateWebAppLink = (slug, client) => {
   const cozyURL = new URL(client.getStackClient().uri)
-  const { cozySubdomainType } = client.getInstanceOptions()
+  const { subdomain } = client.getInstanceOptions()
   const link = generateWebLink({
     cozyUrl: cozyURL.origin,
     slug,
-    subDomainType: cozySubdomainType
+    subDomainType: subdomain
   })
 
   return link
