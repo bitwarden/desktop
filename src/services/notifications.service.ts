482a4af12a216e1cc996ef61b5bf23fbe460cdb1
Yannick Chiron <yannick.chiron@gmail.com>
2021-09-13 16:25:37 +0200

    feat: add waiting mechanism to Cozy realtime when sync is in progress
    Cozy realtime mechanism may collide with Bitwarden's notification
    service
    
    To lower the risk a wait mechanism is implemented and allows to wait
    for sync mechanism to complete before calling realtime handlers

--- src/services/notifications.service.ts
+++ src/services/notifications.service.ts
@@ -1,21 +1,27 @@
 import { ApiService } from 'jslib/abstractions/api.service';
 import { AppIdService } from 'jslib/abstractions/appId.service';
+import { BroadcasterService } from 'jslib/abstractions/broadcaster.service';
 import { EnvironmentService } from 'jslib/abstractions/environment.service';
 import { LogService } from 'jslib/abstractions/log.service';
-import { SyncService } from '../services/sync.service';
 import { UserService } from 'jslib/abstractions/user.service';
 import { VaultTimeoutService } from 'jslib/abstractions/vaultTimeout.service';
+
+import { SyncService } from '../services/sync.service';
 
 import { CozyClientService } from '../cozy/services/cozy-client.service';
 
 import { NotificationsService as NotificationsServiceBase } from 'jslib/services/notifications.service';
+
+import * as uuid from 'uuid';
+
+const BroadcasterSubscriptionId = 'NotificationsService';
 
 export class NotificationsService extends NotificationsServiceBase {
     constructor(userService: UserService, private localSyncService: SyncService,
         appIdService: AppIdService, apiService: ApiService,
         vaultTimeoutService: VaultTimeoutService,
         logoutCallback: () => Promise<void>, logService: LogService,
-        private clientService: CozyClientService) {
+        private clientService: CozyClientService, protected broadcasterService: BroadcasterService) {
             super(
                 userService,
                 localSyncService,
@@ -40,15 +46,40 @@ export class NotificationsService extends NotificationsServiceBase {
         await realtime.subscribe('created', organizationDoctype, this.handleOrganizationCreate.bind(this));
     }
 
+    /**
+     * Wait for the Bitwarden's synchronization to ends
+     * This allows to lower chances of collisions with sync from native notification service
+     */
+    async waitForSyncCompleted() {
+        if (!this.localSyncService.syncInProgress) {
+            return;
+        }
+
+        const listenerUUID = BroadcasterSubscriptionId + uuid.v1();
+        return new Promise<void>(resolve => {
+            this.broadcasterService.subscribe(listenerUUID, (message: any) => {
+                switch (message.command) {
+                    case 'syncCompleted':
+                        this.broadcasterService.unsubscribe(listenerUUID);
+                        resolve();
+                        break;
+                }
+            });
+        });
+    }
+
     async handleOrganizationDelete(organization: any) {
+        await this.waitForSyncCompleted();
         await this.localSyncService.syncDeleteOrganization(organization._id);
     }
 
     async handleOrganizationCreate(organization: any) {
+        await this.waitForSyncCompleted();
         await this.localSyncService.syncUpsertOrganization(organization._id, false);
     }
 
     async handleOrganizationUpdate(organization: any) {
+        await this.waitForSyncCompleted();
         await this.localSyncService.syncUpsertOrganization(organization._id, false);
     }
 }
