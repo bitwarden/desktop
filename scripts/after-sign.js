7634749cbd18f798cecdbda56565ea75ac36704c
Hinton <hinton.oscar@gmail.com>
2021-10-29 11:10:03 +0200

    Add support for re-signing mas

--- scripts/after-sign.js
+++ scripts/after-sign.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const path = require('path');
 const fse = require('fs-extra');
 const { notarize } = require('electron-notarize');
+const { deepAssign } = require('builder-util');
 
 exports.default = run;
 
@@ -9,23 +10,32 @@ async function run(context) {
     console.log('## After sign');
     // console.log(context);
 
-    const macPlatformNames = ['darwin', 'mas'];
-
     const appName = context.packager.appInfo.productFilename;
     const appPath = `${context.appOutDir}/${appName}.app`;
-    const macBuild = macPlatformNames.includes(context.electronPlatformName);
+    const macBuild = context.electronPlatformName === 'darwin';
+    const copyPlugIn = ['darwin', 'mas'].includes(context.electronPlatformName);
 
-    if (macBuild) {
+    if (copyPlugIn) {
+        console.log(context.packager.platformSpecificBuildOptions);
         // Copy Safari plugin to work-around https://github.com/electron-userland/electron-builder/issues/5552
         const plugIn = path.join(__dirname, '../PlugIns');
         if (fse.existsSync(plugIn)) {
             fse.mkdirSync(path.join(appPath, 'Contents/PlugIns'));
             fse.copySync(path.join(plugIn, 'safari.appex'), path.join(appPath, 'Contents/PlugIns/safari.appex'));
-        
-            // Resign to sign safari extension
-            await context.packager.signApp(context, true);
-        }
 
+            // Resign to sign safari extension
+            if (context.electronPlatformName === 'mas') {
+                const masBuildOptions = deepAssign({}, context.packager.platformSpecificBuildOptions, context.packager.config.mas);
+                if (context.packager.packagerOptions.prepackaged == null) {
+                    await context.packager.sign(appPath, context.appOutDir, masBuildOptions, context.arch);
+                }
+            } else {
+                await context.packager.signApp(context, true);
+            }
+        }
+    }
+
+    if (copyPlugIn) {
         console.log('### Notarizing ' + appPath);
         const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
         const appleIdPassword = process.env.APPLE_ID_PASSWORD || `@keychain:AC_PASSWORD`;
