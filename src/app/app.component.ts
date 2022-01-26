e921b483a4b5109820074201bd2501f9123a358b
Vincent Salucci <vincesalucci21@gmail.com>
2022-01-25 20:50:00 -0600

    Revert to using base bwi class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,7 +66,7 @@ const SyncInterval = 6 * 60 * 60 * 1000; // 6 hours
     <app-header></app-header>
     <div id="container">
       <div class="loading" *ngIf="loading">
-        <i class="bwi-spinner bwi-spin bwi-3x" aria-hidden="true"></i>
+        <i class="bwi bwi-spinner bwi-spin bwi-3x" aria-hidden="true"></i>
       </div>
       <router-outlet *ngIf="!loading"></router-outlet>
     </div>
