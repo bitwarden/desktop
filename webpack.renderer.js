a7c5f1ad456bef1de7d2b0e6865c52d56d896735
Oscar Hinton <oscar@oscarhinton.com>
2022-01-27 19:36:50 +0100

    Fix webpack using double dots (#1272)

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -21,7 +21,7 @@ const common = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         exclude: /.*(fontawesome-webfont)\.svg/,
         generator: {
-          filename: "images/[name].[ext]",
+          filename: "images/[name][ext]",
         },
         type: "asset/resource",
       },
@@ -76,7 +76,7 @@ const renderer = {
         test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         exclude: /loading.svg/,
         generator: {
-          filename: "fonts/[name].[ext]",
+          filename: "fonts/[name][ext]",
         },
         type: "asset/resource",
       },
