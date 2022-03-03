97b7236acb1d2b836e52a2b9bca15029af1aa324
Hinton <oscar@oscarhinton.com>
2022-03-03 13:05:49 +0100

    Update readme and cleanup

--- desktop_native/src/lib.rs
+++ desktop_native/src/lib.rs
@@ -4,44 +4,6 @@ extern crate napi_derive;
 mod biometric;
 mod password;
 
-/*
-fn supports_biometric(mut cx: FunctionContext) -> JsResult<JsPromise> {
-    let promise = cx
-        .task(move || Runtime::new().unwrap().block_on(biometric::available()))
-        .promise(|mut cx, n| Ok(cx.boolean(n)));
-
-    Ok(promise)
-}
-
-fn verify(mut cx: FunctionContext) -> JsResult<JsPromise> {
-    let message = cx.argument::<JsString>(0)?.value(&mut cx);
-    let window_handle = cx.argument::<JsNumber>(1)?.value(&mut cx) as isize;
-
-    let promise = cx
-        .task(move || {
-            Runtime::new()
-                .unwrap()
-                .block_on(biometric::verify(&message, window_handle))
-                .unwrap()
-        })
-        .promise(|mut cx, n| Ok(cx.boolean(n)));
-
-    Ok(promise)
-}
-
-#[neon::main]
-fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("verify", verify)?;
-    cx.export_function("supportsBiometric", supports_biometric)?;
-    Ok(())
-}
-
-*/
-
-fn main() {
-    println!("HI");
-}
-
 #[allow(dead_code)]
 #[napi]
 mod passwords {
@@ -54,7 +16,7 @@ mod passwords {
     }
 
     /// Save the password to the keychain. Adds an entry if none exists otherwise updates the existing entry.
-    //#[napi]
+    #[napi]
     pub async fn set_password(
         service: String,
         account: String,
