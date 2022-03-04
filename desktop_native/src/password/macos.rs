d03917481d60ecb98f792d36f8fdcb93dce2c27c
Hinton <oscar@oscarhinton.com>
2022-03-04 18:15:05 +0100

    Fix mac build

--- desktop_native/src/password/macos.rs
+++ desktop_native/src/password/macos.rs
@@ -10,7 +10,7 @@ pub async fn get_password<'a>(service: &str, account: &str) -> Result<String> {
 }
 
 pub async fn get_password_keytar<'a>(service: &str, account: &str) -> Result<String> {
-    get_password(service, account)
+    get_password(service, account).await
 }
 
 pub async fn set_password(service: &str, account: &str, password: &str) -> Result<()> {
