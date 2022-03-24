b688b7543deab82075aa739752a83337dfc5a394
Kyle Spearrin <kyle.spearrin@gmail.com>
2022-03-24 16:28:35 -0400

    resolve build errors with latest jslib ref

--- src/app/vault/password-generator.component.ts
+++ src/app/vault/password-generator.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 import { PasswordGeneratorComponent as BasePasswordGeneratorComponent } from "jslib-angular/components/password-generator.component";
 import { I18nService } from "jslib-common/abstractions/i18n.service";
 import { PasswordGenerationService } from "jslib-common/abstractions/passwordGeneration.service";
 import { PlatformUtilsService } from "jslib-common/abstractions/platformUtils.service";
+import { StateService } from "jslib-common/abstractions/state.service";
+import { UsernameGenerationService } from "jslib-common/abstractions/usernameGeneration.service";
 
 @Component({
   selector: "app-password-generator",
@@ -12,9 +15,20 @@ import { PlatformUtilsService } from "jslib-common/abstractions/platformUtils.se
 export class PasswordGeneratorComponent extends BasePasswordGeneratorComponent {
   constructor(
     passwordGenerationService: PasswordGenerationService,
+    usernameGenerationService: UsernameGenerationService,
+    stateService: StateService,
     platformUtilsService: PlatformUtilsService,
-    i18nService: I18nService
+    i18nService: I18nService,
+    route: ActivatedRoute
   ) {
-    super(passwordGenerationService, platformUtilsService, i18nService, window);
+    super(
+      passwordGenerationService,
+      usernameGenerationService,
+      platformUtilsService,
+      stateService,
+      i18nService,
+      route,
+      window
+    );
   }
 }
