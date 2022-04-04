749bf7436da40d55e9f78c2522587fa5d4e44d4e
Hinton <oscar@oscarhinton.com>
2022-04-04 16:36:44 +0200

    Adjust () to {}

--- desktop_native/src/password/windows.rs
+++ desktop_native/src/password/windows.rs
@@ -145,7 +145,7 @@ mod tests {
 
     #[test]
     fn test() {
-        scopeguard::defer!(delete_password("BitwardenTest", "BitwardenTest").unwrap_or(()););
+        scopeguard::defer!(delete_password("BitwardenTest", "BitwardenTest").unwrap_or({}););
         set_password("BitwardenTest", "BitwardenTest", "Random").unwrap();
         assert_eq!(
             "Random",
@@ -156,7 +156,7 @@ mod tests {
 
     #[test]
     fn test_get_password_keytar() {
-        scopeguard::defer!(delete_password("BitwardenTest", "BitwardenTest").unwrap_or(()););
+        scopeguard::defer!(delete_password("BitwardenTest", "BitwardenTest").unwrap_or({}););
         keytar::set_password("BitwardenTest", "BitwardenTest", "HelloFromKeytar").unwrap();
         assert_eq!(
             "HelloFromKeytar",
