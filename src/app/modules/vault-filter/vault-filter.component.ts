019f80309a0d93df0e667073a4dfc457dccf4c0e
addison <addisonbeck1@gmail.com>
2022-04-26 07:47:49 -0400

    [bug] Remove unused VaultFilterComponent constructor

--- src/app/modules/vault-filter/vault-filter.component.ts
+++ src/app/modules/vault-filter/vault-filter.component.ts
@@ -1,14 +1,9 @@
 import { Component } from "@angular/core";
 
 import { VaultFilterComponent as BaseVaultFilterComponent } from "jslib-angular/modules/vault-filter/vault-filter.component";
-import { VaultFilterService } from "jslib-angular/modules/vault-filter/vault-filter.service";
 
 @Component({
   selector: "app-vault-filter",
   templateUrl: "vault-filter.component.html",
 })
-export class VaultFilterComponent extends BaseVaultFilterComponent {
-  constructor(vaultFilterService: VaultFilterService) {
-    super(vaultFilterService);
-  }
-}
+export class VaultFilterComponent extends BaseVaultFilterComponent {}
